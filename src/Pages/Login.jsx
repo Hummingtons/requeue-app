2522567004eb1c7de78bb7b642bffaad8dcf0c92
Hummingtons <21jmpulmones@gmail.com>
2023-12-14 20:25:39 +0800

    Mergeing Sidebar Component, Inventory.jsx, Login.jsx and DashboardAdmin.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-
+//mergeing Sidebar Component, Inventory.jsx, Login.jsx and DashboardAdmin.jsx
 function Login() {
     const [values, setValues] = useState({
         user_idnum: '',
